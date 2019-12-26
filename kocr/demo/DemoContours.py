df37b0d3d4d43fca46d161b0beb8f55d9afca9b3
核心力量 <kernelforce@gmail.com>
2019-12-26 23:42:54 +0800

    针对短竖线调整自适应精度

--- kocr/demo/DemoContours.py
+++ kocr/demo/DemoContours.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def imshow(img, name=''):
     cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyWindow(name)
+    # plt.imshow(img), plt.title(name)
+    # plt.xticks([]), plt.yticks([])
+    # plt.show()
 
 
 img_file = '../../img/201912082029.png'
@@ -16,17 +20,18 @@ img_origin = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
 img_inverse = cv2.bitwise_not(img_origin)
 
 img_adapt = cv2.adaptiveThreshold(img_inverse, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2)
+# imshow(img_adapt)
 
-img_conn = img_adapt.copy()
-lines = cv2.HoughLinesP(img_conn, 1, np.pi / 180, 100, minLineLength=50, maxLineGap=10)
+img_depict = img_adapt.copy()
+lines = cv2.HoughLinesP(img_depict, 1, np.pi / 180, 100, minLineLength=30, maxLineGap=10)
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(img_conn, (x1, y1), (x2, y2), (255, 0, 0), 1)
-imshow(img_conn)
+    cv2.line(img_depict, (x1, y1), (x2, y2), (255, 255, 255), 1)
+imshow(img_depict)
 
-scale = 5.0
+scale = 10.0
 
-horizontal = img_conn.copy()
+horizontal = img_depict.copy()
 # imshow(horizontal)
 horizontalSize = int(width / scale)
 horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontalSize, 1))
@@ -35,13 +40,18 @@ horizontal = cv2.erode(horizontal, horizontalStructure)
 horizontal = cv2.dilate(horizontal, horizontalStructure)
 # imshow(horizontal)
 
-vertical = img_conn.copy()
+vertical = img_depict.copy()
+imshow(vertical)
 verticalSize = int(height / scale)
 verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, verticalSize))
 vertical = cv2.erode(vertical, verticalStructure)
+imshow(vertical)
 vertical = cv2.dilate(vertical, verticalStructure)
-# imshow(vertical)
+imshow(vertical)
 
 imshow(cv2.bitwise_or(horizontal, vertical))
+cv2.imwrite('../../img/yyy.png', cv2.bitwise_or(horizontal, vertical))
 
-imshow(cv2.bitwise_and(horizontal, vertical))
+# imshow(cv2.bitwise_and(horizontal, vertical))
+cv2.imwrite('../../img/zzz.png', cv2.bitwise_and(horizontal, vertical))
+
