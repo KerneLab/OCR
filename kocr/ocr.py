b0d00091c4b435f71eefa655aea4ebda8afb5217
核心力量 <kernelforce@gmail.com>
2020-02-17 22:47:00 +0800

    Add refine_image method

--- kocr/ocr.py
+++ kocr/ocr.py
@@ -4,6 +4,11 @@ from PIL import Image
 from crnn.crnn_torch import crnnOcr as crnnOcr
 
 
+def enhance_contrast(img_color):
+    # return cv2.normalize(img_color, dst=None, alpha=200, beta=10, norm_type=cv2.NORM_MINMAX)
+    return cv2.createCLAHE(clipLimit=40, tileGridSize=(8, 8)).apply(cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY))
+
+
 def sharpen_image(image):
     kernel_sharpen = np.array([
         [-1, -1, -1, -1, -1],
@@ -15,9 +20,14 @@ def sharpen_image(image):
     return cv2.filter2D(image, -1, kernel_sharpen)
 
 
+def refine_image(img_color):
+    return sharpen_image(enhance_contrast(img_color))
+
+
 def recognize_box(image, box):
     # box = (22, 36, 246, 70)   # Left Top Right Bottom
-    image = Image.fromarray(cv2.cvtColor(sharpen_image(image), cv2.COLOR_BGR2RGB))
+    refine = refine_image(image)
+    image = Image.fromarray(refine)
     sub = image.crop(box)
     text = crnnOcr(sub.convert('L'))
     return text
